372e6aaf26abe4728103288339d9c13417ef8e12
Artem <107567732+artemlevv@users.noreply.github.com>
2024-06-19 16:42:22 +0300

    init

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("Camry", 2021);
+        System.out.println("Hello world!");
+        System.out.println(car);
+    }
+}
